ffefa7bd0a76ec4b7d1fb694dfbd15d316288a23
Padas olga <padas.oa@phystech.edu>
2022-03-25 20:52:25 +0300

    Renaming, error correction, simple tests

--- padas_o_a/build/prj.lab/rational/CMakeFiles/new_target.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/new_target"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/new_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
