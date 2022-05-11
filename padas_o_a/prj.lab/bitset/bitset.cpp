bf0b151215447b04dc4c725da7784d63b552c595
Padas olga <padas.oa@phystech.edu>
2022-05-12 02:32:51 +0300

    First implementation

---
+++ padas_o_a/prj.lab/bitset/bitset.cpp
@@ -0,0 +1,145 @@
+#include <bitset.h>
+#include <stdexcept>
+#include <vector>
+#include <string>
+
+BitSet::BitSet(const int length, const bool default_value) {
+    if (length <= 0) {
+        throw std::out_of_range("Length can't be <= 0");
+    }
+    size = length;
+    data = std::vector<uint8_t>((length + block_size - 1) / block_size, default_value);
+}
+
+BitSet& BitSet::operator|=(const BitSet& other) {
+    if (Size() != other.Size()) {
+        throw std::out_of_range("Sizes must be the same");
+    }
+    for (long unsigned int i = 0; i < data.size(); ++i) {
+        data[i] |= other.data[i];
+    }
+    return *this;
+}
+
+BitSet& BitSet::operator^=(const BitSet& other) {
+    if (Size() != other.Size()) {
+        throw std::out_of_range("Sizes must be the same");
+    }
+    for (long unsigned int i = 0; i < data.size(); ++i) {
+        data[i] ^= other.data[i];
+    }
+    return *this;
+}
+
+
+BitSet& BitSet::operator&=(const BitSet& other) {
+    if (Size() != other.Size()) {
+        throw std::out_of_range("Sizes must be the same");
+    }
+    for (long unsigned int i = 0; i < data.size(); ++i) {
+        data[i] &= other.data[i];
+    }
+    return *this;
+}
+
+const BitSet BitSet::operator~() const {
+    BitSet inverted(size, false);
+    for (long unsigned int i = 0; i < data.size(); ++i) {
+        uint8_t result = ~data[i];
+        inverted[i] = result;
+    }
+    return inverted;
+}
+
+int BitSet::Size() const {
+    return size;
+}
+
+void BitSet::Resize(const int new_size) { 
+    if (new_size <= 0) {
+        throw std::out_of_range("Size must be >= 0");
+    }
+    size = new_size;
+    data.resize((new_size + block_size - 1) / block_size, ~0);
+}
+
+void BitSet::Fill(const bool value) {
+    uint8_t to_fill = value ? ~0 : 0; //попробовать 255
+    for (long unsigned int i = 0; i < data.size(); ++i) {
+        data[i] = to_fill;
+    }
+}
+
+BitSet::BitHolder BitSet::operator[](const int idx) {
+    if (idx < 0 || idx >= size) {
+        throw std::out_of_range("Invalid index");
+    }
+    return BitHolder(*this, idx);
+}
+
+bool BitSet::operator[](const int idx) const {
+    if (idx < 0 || idx >= size) {
+        throw std::out_of_range("Invalid index");
+    }
+    int index = idx / block_size;
+    int offset = idx - index * block_size;
+    return data[index] & (1 << offset);
+}
+
+BitSet::BitHolder& BitSet::BitHolder::operator=(bool value) {
+    owner[offset] = value; // не должно работать
+    return *this;
+}
+
+BitSet::BitHolder::operator bool() const {
+    return owner[offset];
+}
+
+const BitSet operator|(const BitSet& first, const BitSet& second) {
+    if (first.Size() == second.Size()) {
+        throw std::out_of_range("Sizes must be the same");
+    }
+    BitSet result = first;
+    return result |= second;
+}
+
+const BitSet operator&(const BitSet& first, const BitSet& second) {
+    if (first.Size() == second.Size()) {
+        throw std::out_of_range("Sizes must be the same");
+    }
+    BitSet result = first;
+    return result &= second;
+}
+
+const BitSet operator^(const BitSet& first, const BitSet& second) {
+    if (first.Size() == second.Size()) {
+        throw std::out_of_range("Sizes must be the same");
+    }
+    BitSet result = first;
+    return result ^= second;
+}
+
+std::ostream& operator<<(std::ostream& ostrm, const BitSet& bs) {
+    for(int i = 0; i < bs.Size(); ++i) {
+        ostrm << bs[i];
+    }
+    return ostrm;
+}
+
+std::istream& operator>>(std::istream& istrm, BitSet& bs) {
+    std::string input;
+    istrm >> input;
+    if (input.size() == 0) {
+        istrm.setstate(std::ios_base::failbit);
+        return istrm;
+    }
+    bs.Resize(input.size());
+    for (long unsigned int i = 0; i < input.size(); ++i) {
+        if (input[i] != '0' || input[i] != '1') {
+            istrm.setstate(std::ios_base::failbit);
+            return istrm;
+        }
+        bs[i] = (input[i] == '1');
+    }
+    return istrm;
+}
--- padas_o_a/prj.lab/bitset/bitset.h
+++ padas_o_a/prj.lab/bitset/bitset.h
@@ -1,53 +1,65 @@
+#include <vector>
+#include <iostream>
+
 class BitSet {
 public:
 
 private:
-  	// можно будет вернуть, нельзя создавать
-  	// auto smth = bset[123];  // compiles
- 	class BitHolder {
-	  public:
-  		BitHolder() = delete;
-
-  		BitHolder(const BitHolder &) = delete;
-  		BitHolder(BitHolder &&) = delete;
-
-  		BitHolder& operator=(const BitHolder &);
-   		BitHolder& operator=(bool);
-  		BitHolder& operator=(BitHolder &&);
-    
-  		operator bool() const;
-  	};
+ 	class BitHolder;
 
 
 public:
-  	BitSet();
-	// except: length <= 0
-  	BitSet(const int length, const bool default_value=false);
- 
-  	BitSet(const BitSet &);
-  	BitSet(BitSet &&);
- 
-  	BitSet& operator=(const BitSet &);
-  	BitSet& operator=(BitSet &&);
+  	BitSet() = default;
+  	BitSet(const BitSet &) = default;
+  	BitSet(BitSet &&) = default;
 	
-	~BitSet();
+	BitSet(const int length, const bool default_value=false);
+ 
+  	BitSet& operator=(const BitSet &) = default;
+  	BitSet& operator=(BitSet &&) = default;
+	
+	~BitSet() = default;
 
-	// except: this->size != other.size
 	BitSet& operator|=(const BitSet& other);
-// except: this->size != other.size
 	BitSet& operator^=(const BitSet& other);
-// except: this->size != other.size
 	BitSet& operator&=(const BitSet& other);
-const BitSet operator~() const;
+	const BitSet operator~() const;
  	
   	int Size() const;
 	
-	// except: length <= 0
   	void Resize(const int new_size);
 	void Fill(const bool value);
  	
-	// except: idx < 0 or idx >= size 
   	BitHolder operator[](const int idx);
-	// except: idx < 0 or idx >= size 
   	bool operator[](const int idx) const;
+
+private:
+	std::vector<uint8_t> data;
+	int size = 0;
+	const int block_size = 16;
+
+	class BitHolder {
+	public:
+  		BitHolder() = delete;
+  		BitHolder(const BitHolder &) = default;
+  		BitHolder(BitHolder &&) = default;
+		BitHolder(BitSet& owner, const int offset) : owner(owner), offset(offset) {}
+
+  		BitHolder& operator=(const BitHolder &) = default;
+   		BitHolder& operator=(bool);
+  		BitHolder& operator=(BitHolder &&) = default;
+    
+  		operator bool() const;
+	private:
+		BitSet& owner;
+		int offset = 0;
+  	};
 };
+
+
+const BitSet operator|(const BitSet& first, const BitSet& second);
+const BitSet operator&(const BitSet& first, const BitSet& second);
+const BitSet operator^(const BitSet& first, const BitSet& second);
+
+std::istream& operator>>(std::istream& istrm, BitSet& bs);
+std::ostream& operator<<(std::ostream& ostrm, const BitSet& bs);
